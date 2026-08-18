68fb1c5cf437995d353febf7ddbcca2136db2d87
dennismenze <123099268+dennismenze@users.noreply.github.com>
2026-08-18 21:13:03 +0200

    fix: load full Omnipod context importer

--- docs/app-v3.js
+++ docs/app-v3.js
@@ -3,7 +3,11 @@
 
   if (typeof document === 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
-      module.exports = require('./app-v3-core.js');
+      module.exports = {
+        ...require('./app-v3-core.js'),
+        ...require('./app-importers.js'),
+        ...require('./app-importers-context.js'),
+      };
     }
     return;
   }
@@ -20,5 +24,7 @@
     document.head.appendChild(script);
   }
 
-  loadScript('app-v3-core.js', () => loadScript('app-importers.js'));
+  loadScript('app-v3-core.js', () =>
+    loadScript('app-importers.js', () => loadScript('app-importers-context.js')),
+  );
 })();
